26fffdff5149f789cdbb2331e892579f8c55f0d2
Sean <seanlengws@gmail.com>
2022-02-05 18:04:44 +0800

    Added prettier config file to standardize code formatting

--- packages/next-app/src/hooks/useContractInfo.ts
+++ packages/next-app/src/hooks/useContractInfo.ts
@@ -6,39 +6,37 @@ import { getContractAddress } from "@/utils";
 import { ethers } from "ethers";
 
 type ContractInfo = {
-  claimPeriodEnds: Date | undefined;
-  merkleRoot: string | undefined;
+    claimPeriodEnds: Date | undefined;
+    merkleRoot: string | undefined;
 };
 
 const contractAddress = getContractAddress();
 
 const useContractInfo = (): ContractInfo => {
-  const [contractInfo, setContractInfo] = useState<ContractInfo>({
-    claimPeriodEnds: undefined,
-    merkleRoot: undefined,
-  });
+    const [contractInfo, setContractInfo] = useState<ContractInfo>({
+        claimPeriodEnds: undefined,
+        merkleRoot: undefined
+    });
 
-  const provider = useProvider();
-  useEffect(() => {
-    const tokenContract = CODEToken__factory.connect(contractAddress, provider);
+    const provider = useProvider();
+    useEffect(() => {
+        const tokenContract = CODEToken__factory.connect(contractAddress, provider);
 
-    const fetchContractInfo = async () => {
-      const claimPeriodEndsRes = await tokenContract.claimPeriodEnds();
-      const claimPeriodEndsDate = new Date(
-        claimPeriodEndsRes.toNumber() * 1000
-      );
+        const fetchContractInfo = async () => {
+            const claimPeriodEndsRes = await tokenContract.claimPeriodEnds();
+            const claimPeriodEndsDate = new Date(claimPeriodEndsRes.toNumber() * 1000);
 
-      const merkleRoot = await tokenContract.merkleRoot();
+            const merkleRoot = await tokenContract.merkleRoot();
 
-      setContractInfo({ claimPeriodEnds: claimPeriodEndsDate, merkleRoot });
-    };
+            setContractInfo({ claimPeriodEnds: claimPeriodEndsDate, merkleRoot });
+        };
 
-    if (provider) {
-      fetchContractInfo();
-    }
-  }, [provider]);
+        if (provider) {
+            fetchContractInfo();
+        }
+    }, [provider]);
 
-  return contractInfo;
+    return contractInfo;
 };
 
 export default useContractInfo;
