0d358c94522c1a332ccb04a321cc1cded4a068e2
Sean <seanlengws@gmail.com>
2022-02-07 22:31:33 +0800

    Added useCompletion hook to get block confirmations

--- packages/next-app/src/pages/index.tsx
+++ packages/next-app/src/pages/index.tsx
@@ -7,49 +7,29 @@ import { ClaimCard } from "@/components/ClaimCard";
 import { Logo } from "@/components/Logo";
 import { MainBox } from "@/components/MainBox";
 import useConfirmations from "@/hooks/useConfirmations";
+import { Confirmations } from "@/components/Confirmations";
 
 const Home: NextPage = () => {
-  const confirmations = useConfirmations()
   const [{ data: networkData, error, loading }] = useNetwork();
   const [{ data: accountData }, disconnect] = useAccount({
     fetchEns: true,
   });
 
-  const isConnected =
-    typeof accountData !== "undefined" &&
-    Object.entries(accountData).length > 0;
+  const isConnected = typeof accountData !== "undefined" && Object.entries(accountData).length > 0;
 
-  console.log(confirmations)
   return (
     <Box m="0" w="100vw" h="100vh" background="blue">
       <Head>
         <title>$CODE Claim Page</title>
       </Head>
       <Flex direction="row" flexWrap="wrap">
-        <Box
-          w={{ base: "100vw", lg: "50vw" }}
-          h="100vh"
-          m="0"
-          pl={["24px", "5vw"]}
-          pr={["40px", "8vw"]}
-          background="#08010D"
-        >
+        <Box w={{ base: "100vw", lg: "50vw" }} h="100vh" m="0" pl={["24px", "5vw"]} pr={["40px", "8vw"]} background="#08010D">
           <Box mt={["32px", "48px"]} mb="22vh">
             <Logo />
           </Box>
-          <MainBox
-            isConnected={isConnected}
-            isUnsupported={!!networkData.chain?.unsupported}
-          />
+          <MainBox isConnected={isConnected} isUnsupported={!!networkData.chain?.unsupported} />
         </Box>
-        <Flex
-          w={{ base: "100vw", lg: "50vw" }}
-          h="100vh"
-          m="0"
-          backgroundColor="#F1F0F5"
-          align="center"
-          justifyContent="center"
-        >
+        <Flex w={{ base: "100vw", lg: "50vw" }} h="100vh" m="0" backgroundColor="#F1F0F5" align="center" justifyContent="center">
           <SlideFade in={isConnected} offsetY="20px">
             <Box m={["24px", "10vw"]}>
               <ClaimCard />
